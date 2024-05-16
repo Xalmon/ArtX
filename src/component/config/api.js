d9796cfee614cd83cc2736f537f36ac5d45b6047
ikjames5454 <ikennajames03@gmail.com>
2024-05-16 14:48:31 +0100

    add

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // export const API_URL = "http://localhost:8080";
 
-export const API_URL = "https://artxpressfontend-d914.onrender.com"
+export const API_URL = "https://artxpressbackend.onrender.com"
 
 export const api = axios.create({
     baseURL:  API_URL,
